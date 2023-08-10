e2e4c4fc75b75f147a8598bd30108e0d71760c7d
xan1242 <8014093+xan1242@users.noreply.github.com>
2023-08-10 10:43:44 +0200

    replace batch script with powershell

--- ee/bin/vsmake.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-set PATH=%~dp0;%PATH%
-(%~dp0make 2>&1 %1 %2 %3 %4 %5 ^& ^>err.tmp call echo %%^^errorlevel%%) | %~dp0sed -e "s/\([^:]*\):\([0-9][0-9]*\)\(.*\)/\1 (\2) \3/"
-set /p "leftErr=" <err.tmp
-del err.tmp
-exit /b %leftErr%
